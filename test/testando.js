cb9ebacae4204609c49ee2556869ff2a9fc56dfb
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-06-04 12:15:19 -0300

    Minor changes

--- test/testando.js
+++ test/testando.js
@@ -3,11 +3,11 @@ var assert = require('assert');
 describe('Testando o funcionamento do Mocha', function(){
 
     it('1 + 1 deve ser igual a 2', function(){
-        assert(1+2, 2);
+        assert.equal(1 + 1, 2);
     });
 
     it('1 + 2 não deve ser igual a 2', function(){
-        assert.notEqual(1+2, 2);
+        assert.notEqual(1 + 2, 2);
     });
 
 });

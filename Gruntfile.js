ff9be0d972d0750ec31e8498034d48e45c853dd4
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-06-10 16:33:42 -0300

    Create a grunt task to copy assets from bower

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        copy: {
+            main: {
+                files: [
+                    {
+                        expand: true,
+                        src: "bower_components/bootstrap/dist/css/*",
+                        dest: "public/stylesheets/bootstrap",
+                        flatten: true
+                    },
+                    {
+                        expand: true,
+                        src: "bower_components/bootstrap/dist/js/*",
+                        dest: "public/javascripts/bootstrap",
+                        flatten: true
+                    },
+                    {
+                        expand: true,
+                        src: "bower_components/jquery/dist/*",
+                        dest: "public/javascripts/jquery",
+                        flatten: true
+                    }
+                ]
+            }
+        }
+    });
+
+  grunt.loadNpmTasks('grunt-contrib-copy');
+};

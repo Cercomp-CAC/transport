7e3c9ed46954a5ae4ac23a0a0d2b664f1d397d2e
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-06-13 16:59:58 -0300

    Add test for login page

---
+++ test/login.js
@@ -0,0 +1,15 @@
+var app     = require('../server'),
+    request = require('supertest')(app),
+    assert  = require('assert');
+
+describe('No controller login', function(){
+
+    it('deve retornar status 200 ao fazer GET /login', function(done) {
+        request
+            .get('/login')
+            .end(function(err, res) {
+                assert.equal(res.status, 200);
+            });
+        done();
+    });
+});

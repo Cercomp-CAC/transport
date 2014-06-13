9b67e947b23e95fed77d2ea796b3041f689da510
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-06-13 20:26:26 -0300

    Minor

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require('express'),
     app     = express();
 
-require("./config/express")(app, express);
-require("./config/routes")(app);
+require('./config/express')(app, express);
+require('./config/routes')(app);
 
 app.listen(3000);
 console.log('Express serving listening on port 3000');

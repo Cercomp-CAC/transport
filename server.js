d3b23cdcdfa625b1d7f34a575cd8918e22c7eb1a
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-06-14 01:30:35 -0300

    Minor

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('./config/express')(app, express);
 require('./config/routes')(app);
 
 var port = Number(process.env.PORT || 3000);
+
 app.listen(port, function() {
     console.log('Express serving listening on port ' + port);
 });

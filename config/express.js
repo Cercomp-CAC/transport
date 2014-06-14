ee06b8fba6ccb5d7374597e6e92dd13a891f4b2e
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-06-14 01:45:08 -0300

    Fix build heroku

--- config/express.js
+++ config/express.js
@@ -1,13 +1,13 @@
 module.exports = function(app, express) {
 
-    var path   = require('path'),
-        logfmt = require('logfmt');
+    // var path   = require('path'),
+    var logfmt = require('logfmt');
 
     // view engine setup
-    app.set('views', path.join(process.env.PWD, 'app/views'));
+    app.set('views', process.env.PWD + '/app/views');
     app.set('view engine', 'jade');
 
-    app.use(express.static(path.join(process.env.PWD, 'public')));
+    app.use(express.static(process.env.PWD + '/public'));
 
     app.use(logfmt.requestLogger());
     // app.use(express.logger('dev'));

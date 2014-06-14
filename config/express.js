ed9d352db860303be84b15cbf8c054a8dae4f4b5
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-06-14 09:30:49 -0300

    Rename assets folders

--- config/express.js
+++ config/express.js
@@ -3,13 +3,13 @@ module.exports = function(app, express) {
     // var path   = require('path'),
     var logfmt = require('logfmt');
 
-    // view engine setup
-    app.set('views', process.env.PWD + '/app/views');
-    app.set('view engine', 'jade');
-
-    app.use(express.static(process.env.PWD + '/public'));
-
-    app.use(logfmt.requestLogger());
-    // app.use(express.logger('dev'));
-    app.use(express.bodyParser());
+    // Configuration
+    app.configure(function(){
+        app.set('views', process.env.PWD + '/app/views');
+        app.set('view engine', 'jade');
+        app.use(express.bodyParser());
+        app.use(express.static(process.env.PWD + '/public'));
+        app.use(logfmt.requestLogger());
+        app.use(app.router);
+    });
 };
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('./config/routes')(app);
 var port = Number(process.env.PORT || 3000);
 
 app.listen(port, function() {
-    console.log('Express serving listening on port ' + port);
+    console.log("Express serving listening on port %d", port);
 });
 
 module.exports = app;

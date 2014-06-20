2d7bd935998486ade2e6c80772504c6b4790b1a4
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-06-20 12:35:32 -0300

    Add support for using angular.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,7 @@ module.exports = function(grunt) {
         src: [
           'bower_components/jquery/dist/jquery.js',
           'bower_components/bootstrap/dist/js/bootstrap.js',
+          'bower_components/angular/angular.js',
           'public/js/main.js'
         ],
         dest: 'public/js/production.js'

6d62bfbd451cb01575f939302c92922d36d50e34
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-06-23 00:38:58 -0300

    Minor

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('./config/routes')(app);
 var port = app.get('port');
 
 app.listen(port, function() {
-  console.log("Express serving listening on port %d in %s mode",
+  console.log('Express serving listening on port %d in %s mode',
     port, app.settings.env);
 });
 

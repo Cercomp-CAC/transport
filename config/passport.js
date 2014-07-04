8f2709356a50fc64e887cbf5fe21aa9dc8c5c83e
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-07-04 12:12:27 -0300

    Clean code

--- config/passport.js
+++ config/passport.js
@@ -20,50 +20,49 @@ module.exports = function (passport) {
   };
 
   // use Local strategy
-  passport.use('local-signup', new LocalStrategy(strategyOptions,
-    function(req, email, password, done) {
+  passport.use('local-signup', new LocalStrategy(strategyOptions, function(req, email, password, done) {
 
-      process.nextTick(function() {
-        User.findOne({ 'email': email }).exec(function(err, user) {
-          if (err) {
-            return done(err);
-          }
-          if (user) {
-            return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-          } else {
-            var newUser = new User();
-
-            newUser.email    = email;
-            newUser.password = newUser.generateHash(password);
-
-            newUser.save(function(err) {
-              if (err) {
-                throw err;
-              }
-              return done(null, newUser, req.flash('signupSuccessfulMessage', 'Your account was created.'));
-            });
-          }
-        });
-      });
-    }
-  ));
-
-  passport.use('local-login', new LocalStrategy(strategyOptions,
-    function(req, email, password, done) {
+    process.nextTick(function() {
 
       User.findOne({ 'email': email }).exec(function(err, user) {
         if (err) {
           return done(err);
         }
-        if (!user) {
-          return done(null, false, req.flash('loginMessage', 'No user found.'));
+        if (user) {
+          return done(null, false, req.flash('signupFailureMessage', 'That email is already taken.'));
+        } else {
+          var newUser = new User();
+
+          newUser.email    = email;
+          newUser.password = newUser.generateHash(password);
+
+          newUser.save(function(err) {
+            if (err) {
+              throw err;
+            }
+            return done(null, newUser, req.flash('signupSuccessfulMessage', 'Your account was created.'));
+          });
         }
-        if (!user.validPassword(password)) {
-          return done(null, false, req.flash('loginMessage', 'Oops! Wrong password'));
-        }
-        // return successful user
-        return done(null, user);
       });
-    }
-  ));
+
+    });
+  }));
+
+  passport.use('local-login', new LocalStrategy(strategyOptions, function(req, email, password, done) {
+
+    User.findOne({ 'email': email }).exec(function(err, user) {
+      if (err) {
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, req.flash('loginMessage', 'No user found.'));
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, req.flash('loginMessage', 'Oops! Wrong password'));
+      }
+      // return successful user
+      return done(null, user);
+    });
+  }));
+
 };

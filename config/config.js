926b48d3b320d287af352b38542e0b9a25a564a8
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-07-05 14:13:57 -0300

    Use config vars for production on heroku

---
+++ config/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  development: {
+    root: process.env.PWD,
+    port: process.env.PORT || 3000,
+    sessionSecret: 'ftshovyjpglkznawcqxdubirme',
+    db: "mongodb://localhost/transport-dev"
+  },
+  production: {
+    root: process.env.PWD,
+    port: process.env.PORT,
+    sessionSecret: process.env.SESSION_SECRET,
+    db: process.env.MONGOLAB_URI
+  }
+};

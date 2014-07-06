77b7d7420da7a63f5f64c35dcb5be7e3ba7b4fb5
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-07-06 19:50:05 -0300

    Remove unused controllers

--- app/controllers/login.js
+++
@@ -1,5 +0,0 @@
-exports.login = function (req, res) {
-  res.render('login/login', {
-    message: req.flash('loginMessage')
-  });
-};
--- app/controllers/signup.js
+++
@@ -1,6 +0,0 @@
-exports.signup = function (req, res) {
-  res.render('signup/signup', {
-    messageFailure: req.flash('signupFailureMessage'),
-    messageSuccess: req.flash('signupSuccessfulMessage'),
-  });
-};
